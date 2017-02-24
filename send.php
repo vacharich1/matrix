463a36426f39baffd9a266b19be49cc78a948d0c
jay <jr30149_@hotmail.com>
2017-02-24 22:59:34 +0700

    fsdfsdf

--- send.php
+++ send.php
@@ -27,9 +27,18 @@ if (!$link) {
 $sql1 = "SELECT * FROM `sendalert_store2` ORDER BY `hoonname` ASC";
 $result = $link->query($sql1);
 if ($result->num_rows > 0) {
-	$messages556=$messages556."alert 3.2 store2\n\n";
+	$count=0;
+	$messages556=$messages556."alert 3.2 store2 ";
 	while($row = $result->fetch_assoc()) {
-		$messages556=$messages556.$row["hoonname"]."  :  ".$row["volume"]."  ".$row["time"]."\n";
+		$count=$count+1;
+		if(count==1)
+		{
+			$messages556=$messages556.$row["time"]."\n\n";
+		}
+		else
+		{
+			$messages556=$messages556.$count." ".$row["hoonname"]."  :  ".$row["volume"]."\n\n";
+		}
 	}
 
 }
